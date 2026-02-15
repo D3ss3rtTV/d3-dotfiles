b232cd54ac5bf946139e48b2267dd668c193f7a8
Thomas Wilson <twilson9182@gmail.com>
2026-02-14 17:37:01 -0700

    Create aliases.fish

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,22 @@
+# Neovim
+alias v="nvim"
+alias vim="nvim"
+
+# Navigation
+alias ll="ls -lah"
+alias la="ls -A"
+alias ..="cd .."
+alias ...="cd ../.."
+
+# Git muscle memory
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+alias gl="git pull"
+alias gco="git checkout"
+
+# Safety
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
